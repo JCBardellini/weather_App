38c40f589b4f83aa421fb4b1d68700f125347975
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2024-04-14 19:04:42 -0700

    highlights js added

--- src/Context/primaryContext.jsx
+++ src/Context/primaryContext.jsx
@@ -7,17 +7,22 @@ const PrimaryProvider = ({ children }) => {
   const weatherApiKey = "df1fd18342690d43086da43326902da5";
 
   const [weatherData, setWeatherData] = useState();
+  const [hourlyData, setHourlyData] = useState();
+  const [airPollutionData, setAirPollutionData] = useState();
   const [cityName, setCityName] = useState("Seattle");
 
   useEffect(() => {
     const dataFromWeatherApi = () => {
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}`;
-      // console.log("API URL:", apiUrl);
+      const hourlyUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherApiKey}`;
       try {
         const data = axios.get(apiUrl).then((res) => {
           const response = res.data;
-          // console.log(response);
           setWeatherData(response);
+        });
+        const hourlyData = axios.get(hourlyUrl).then((res) => {
+          const hourlyResponse = res.data;
+          const hourlyListData = hourlyResponse.list.slice(2, 8);
         });
       } catch (error) {
         console.error("error in obtaining the API call" + error);
@@ -26,7 +31,28 @@ const PrimaryProvider = ({ children }) => {
     dataFromWeatherApi();
   }, [cityName, weatherApiKey]);
 
-  console.log(weatherData);
+  useEffect(() => {
+    if (weatherData) {
+      // Only perform this logic when weatherData is available
+      const { coord } = weatherData;
+      if (coord) {
+        const { lon, lat } = coord;
+        // console.log(lon, lat);
+        const airPollutionUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+        try {
+          axios.get(airPollutionUrl).then((res) => {
+            const airPollutionResponse = res.data;
+            // console.log(airPollutionResponse);
+            setAirPollutionData(airPollutionResponse);
+          });
+        } catch (error) {
+          console.error("Error in obtaining the air pollution data:", error);
+        }
+      }
+    }
+  }, [weatherData, weatherApiKey]);
+
+  // console.log(weatherData);
   return (
     <primaryContext.Provider
       value={{
@@ -34,6 +60,10 @@ const PrimaryProvider = ({ children }) => {
         setCityName,
         weatherData,
         setWeatherData,
+        airPollutionData,
+        setAirPollutionData,
+        hourlyData,
+        setHourlyData,
       }}
     >
       {children}
--- src/components/highlights/Highlights.jsx
+++ src/components/highlights/Highlights.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Wind from "../../assets/icons/wind.png";
 import Sunrise from "../../assets/icons/sunrise.png";
 import Sunset from "../../assets/icons/sunset.png";
@@ -7,8 +7,37 @@ import Pressure from "../../assets/icons/pressure.png";
 import Visibility from "../../assets/icons/visibility.png";
 import FeelsLike from "../../assets/icons/thermometer.png";
 import "./hightlights.css";
+import { primaryContext } from "../../Context/primaryContext";
 
 const Highlights = () => {
+  const { weatherData, airPollutionData } = useContext(primaryContext);
+  // console.log(weatherData);
+  // console.log(airPollutionData);
+  const pollutionData = airPollutionData.list[0].components;
+  console.log(pollutionData);
+
+  const meterToMiles = 0.000621371;
+  const visibility = weatherData.visibility * meterToMiles;
+  const sunrise = weatherData.sys.sunrise;
+  const sunset = weatherData.sys.sunset;
+  const inHg = 0.02953;
+  const pressure = weatherData.main.pressure * inHg;
+  const feelsLike = Math.round(
+    ((weatherData.main.feels_like - 273.15) * 9) / 5 + 32
+  );
+
+  const convertTimestamp = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    const formattedMinutes = minutes.substring(minutes.length - 2);
+    const formattedTime =
+      hours >= 12
+        ? `${hours % 12}:${formattedMinutes} PM`
+        : `${hours}:${formattedMinutes} AM`;
+    return formattedTime;
+  };
+
   return (
     <section className="overallContainerHighlights">
       <h3>Today's Highlight</h3>
@@ -17,12 +46,12 @@ const Highlights = () => {
           <h4>Pollutants</h4>
           <div className="eachPollutantContainer">
             <img src={Wind} alt="wind Icon" className="icons" />
-            <p>Good</p>
+
             <ul>
-              <li>O3: num</li>
-              <li>SO2: num</li>
-              <li>NO2: num</li>
-              <li>CO: num</li>
+              <li>O3: {pollutionData.o3}</li>
+              <li>SO2: {pollutionData.so2}</li>
+              <li>NO2: {pollutionData.no2}</li>
+              <li>CO: {pollutionData.co}</li>
             </ul>
           </div>
         </div>
@@ -31,11 +60,11 @@ const Highlights = () => {
           <div className="time">
             <div className="timeHighlight">
               <img src={Sunrise} alt="Sunrise Icon" className="icons" />
-              <p>Time</p>
+              <p>{convertTimestamp(sunrise)}</p>
             </div>
             <div className="timeHighlight">
               <img src={Sunset} alt="Sunset Icon" className="icons" />
-              <p>Time</p>
+              <p>{convertTimestamp(sunset)}</p>
             </div>
           </div>
         </div>
@@ -43,28 +72,28 @@ const Highlights = () => {
           <h4>Humidity</h4>
           <div className="eachHighlight">
             <img src={Humidity} alt="Humidity Icon" className="icons" />
-            <p>88%</p>
+            <p>{weatherData.main.humidity}%</p>
           </div>
         </div>
         <div className="eachHighlightContainer">
           <h4>Pressure</h4>
           <div className="eachHighlight">
             <img src={Pressure} alt="Pressure Icon" className="icons" />
-            <p>30.03inHg</p>
+            <p>{pressure.toFixed(2)} inHg</p>
           </div>
         </div>
         <div className="eachHighlightContainer">
-          <h4>Pressure</h4>
+          <h4>Visibility</h4>
           <div className="eachHighlight">
             <img src={Visibility} alt="Visibility Icon" className="icons" />
-            <p>30.03inHg</p>
+            <p>{visibility.toFixed(2)} mi</p>
           </div>
         </div>
         <div className="eachHighlightContainer">
           <h4>Feels Like</h4>
           <div className="eachHighlight">
             <img src={FeelsLike} alt="Temperature Icon" className="icons" />
-            <p>30.03inHg</p>
+            <p>{feelsLike}&deg;</p>
           </div>
         </div>
       </div>
